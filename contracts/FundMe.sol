ede70b5efbbe0d57371715d680cb296f48e86395
Wakushi <mzozord@gmail.com>
2023-04-10 11:15:15 +0200

    first commit (prev update of ethers + hardhat)

---
+++ contracts/FundMe.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+// Pragma statements
+pragma solidity ^0.8.8;
+
+// Import statements
+import "./PriceConverter.sol";
+
+// Error codes
+error FundMe__NotOwner();
+
+// Interfaces, Libraries, Contracts
+
+/**
+ * @title A contract for crowd funding
+ * @author Wakushi
+ * @notice This contract is to demo a sample funding contract
+ * @dev This implements price feeds as our library
+ */
+
+contract FundMe {
+    // Type Declarations
+    using PriceConverter for uint256;
+
+    // State variables
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+    address public immutable i_owner;
+    AggregatorV3Interface public priceFeed;
+
+    // Modifiers
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
+    }
+
+    // Functions
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        // AggregatorV3Interface returns an instance of the interface we use to get the price feed
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    /**
+     * @notice This function funds this contract
+     * @dev This implements price feeds as our library
+     */
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
+            "Didn't send enough!"
+        );
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed!");
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,53 @@
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+require("@nomiclabs/hardhat-ethers")
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+    // solidity: "0.8.8",
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        users: {
+            default: 0,
+        },
+    },
+}
+
+// Sepolia priceFeed = 0x694AA1769357215DE4FAC081bf1f309aDC325306
